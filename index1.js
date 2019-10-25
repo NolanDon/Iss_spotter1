5a09ce23632013a0338ddca0ea588cfc50c636c1
Nolan Don <48977789+NolanDon@users.noreply.github.com>
2019-10-24 23:09:13 -0600

    Update index1.js

--- index1.js
+++ index1.js
@@ -1,22 +1,16 @@
 const { nextISSTimesForMyLocation } = require('./iss_promised')
-
-
-
-nextISSTimesForMyLocation()
+  nextISSTimesForMyLocation()
   .then((passover) => {
     comingSoon(passover)
-  })
+    })
   .catch((error) =>{
     console.log("It didn't work: ", error.message)
   })
   const comingSoon = function (passover) {
-  
     for (nextPass of passover) {
       const realtime = new Date(0);//credit help at end of page
       realtime.setUTCSeconds(nextPass.risetime);
       const minutes = Math.round(nextPass.duration / 60)
-     
-      console.log(`The next pass will be at ${realtime} and last ${minutes} minutes. SO GUTCHA POPCORN READY!`)  
-    }
-  
-  }
+    console.log(`The next pass will be at ${realtime} and last ${minutes} minutes. SO GUTCHA POPCORN READY!`)  
+   }
+ };
